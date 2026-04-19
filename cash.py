a50d462e04e7061a3292e61bfee61de6d90db507
Felipe Felix <lipegfelix@gmail.com>
2026-04-19 18:11:37 -0300

    versão atualizada

--- cash.py
+++ cash.py
@@ -7,44 +7,55 @@ def limpar():
 lista_receita = []
 lista_despesa = []
 
-def receita():
-    valor1 = float(input("Insira o valor da receita: "))
-    valor2 = input("Insira o tipo da receita: ")
-    valor_receita = (valor1, valor2)
-    lista_receita.append(valor_receita)
-    retorno()
+def inserir_receita():
+    descricao = input("Insira a descrição da receita: ")
+    valor = float(input("Insira o valor da receita: "))
+    item_receita = {
+        "Descricao" : descricao,
+        "Valor" : valor
+    }
+    lista_receita.append(item_receita)
 
-def despesa():
-    valor1 = float(input("Insira o valor da despesa: "))
-    valor2 = input("Insira o tipo da despesa: ")
-    valor_despesa = (valor1, valor2)
-    lista_despesa.append(valor_despesa)
-    retorno()
+def inserir_despesa():
+    descricao = input("Insira a descrição da despesa: ")
+    valor = float(input("Insira o valor da despesa: "))
+    item_despesa = {
+        "Descricao" : descricao,
+        "Valor" : valor
+    }
+    lista_despesa.append(item_despesa)
+    #retorno()
 
-def retorno():
-    input("Digite qualquer valor para voltar: ")
-    limpar()
-    main()
+def imprimir_receitas():
+    for i in lista_receita:
+        print("-----------------------------")
+        print("Descrição: ", i['Descricao'])
+        print("Valor: ", i["Valor"])
+
+def imprimir_despesas():
+    for i in lista_despesa:
+        print("------------------------------")
+        print("Descrição: ", i["Descricao"])
+        print("Valor: ", i["Valor"])
+
 
 def main():
     resposta = ""
     
     while resposta != 5:
-        resposta = int(input("Selecione uma das opções:\n 1 - Cadastrar receita\n 2 - Cadastrar despesa\n 3 - Abrir receitas\n 4 - Abrir despesas\n 5 - Sair\n-> "))
+        resposta = int(input("Selecione uma das opções:\n 1 - Cadastrar receita\n 2 - Cadastrar despesa\n 3 - Imprimir receitas\n 4 - Imprimir despesas\n 5 - Sair\n-> "))
 
         if resposta == 1:
-            receita()
+            inserir_receita()
 
         elif resposta == 2:
-            despesa()
+            inserir_despesa()
 
         elif resposta == 3:
-            print(lista_receita)
-            retorno()
+            imprimir_receitas()
 
         elif resposta == 4:
-            print(lista_despesa)
-            retorno()
+            imprimir_despesas()
 
         elif resposta == 5:
             limpar()
@@ -53,7 +64,9 @@ def main():
 
         else:
             print("Escreva uma opção válida")
-            retorno()
+
+        input("Digite qualquer valor para voltar: ")
+        limpar()
 
 
 if __name__ == "__main__":
